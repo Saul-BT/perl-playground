418ea0d76a2436d53ba6df019adbd4194857d6bd
Saul Blanco Tejero <alu15067@iesgalileo.es>
2019-09-11 19:32:16 +0200

    Analisis textual refactorizado

--- testing/analisis-texto.pl
+++ testing/analisis-texto.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl
 
 print "What are you looking for? ";
+$numResults = 0;
 $word = <STDIN>;
 chomp($word);
 
 for $line (<>) {
   if ($line =~ m/\b$word\b/i) {
+    $numResults += 1;
     print "[$word FOUND]> $line\n";
+    next;
   }
-  else {print $line;}
+  print $line;
 }
+
+print "\n\n=== There are $numResults coincidences ==="
